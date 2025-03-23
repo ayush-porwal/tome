b6870334e836c055777f669559a8e56fe48377ca
Ayush Porwal <ayush-porwal@lxuser-HP-ProBook-450-15-6-inch-G10-Notebook-PC>
2025-03-23 18:49:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TOME
+
+A chrome extension which helps you save the articles you want to read in future
---
+++ entrypoints/popup/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex">
+      <h1>Tome</h1>
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default App;
